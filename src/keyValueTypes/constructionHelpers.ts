5c16c93a677b19ae6a5a03ffa47e1960cc0ec465
Stefan Neubert <Stefan.Neubert@student.hpi.de>
2019-12-14 15:25:31 +0100

    Mark construction helper arguments readonly

--- src/keyValueTypes/constructionHelpers.ts
+++ src/keyValueTypes/constructionHelpers.ts
@@ -6,12 +6,15 @@ export interface KeyValueEntryConstructionData {
     value: string | any;
 }
 
-export function createEntriesFromTypes(entries: KeyValueEntryConstructionData[], types: Array<KeyValueType<any>>) {
+export function createEntriesFromTypes(
+    entries: ReadonlyArray<Readonly<KeyValueEntryConstructionData>>,
+    types: ReadonlyArray<KeyValueType<any>>,
+) {
     return entries.map((e) => createEntry(e, types))
         .filter((entry) => entry !== undefined) as Array<KeyValueEntry<any>>;
 }
 
-function createEntry(e: KeyValueEntryConstructionData, types: Array<KeyValueType<any>>)
+function createEntry(e: Readonly<KeyValueEntryConstructionData>, types: ReadonlyArray<KeyValueType<any>>)
     : KeyValueEntry<any> | undefined {
     for (const type of types) {
         if (type.id === e.key) {
